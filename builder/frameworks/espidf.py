85e831a9da2114cf005f0b698779d0b2d0ff0063
Jonathan Swoboda <154711427+swoboda1337@users.noreply.github.com>
2025-06-05 08:46:29 -0400

    Add rich dep,

--- builder/frameworks/espidf.py
+++ builder/frameworks/espidf.py
@@ -1539,6 +1539,7 @@ def install_python_deps():
         # https://github.com/platformio/platform-espressif32/issues/635
         "cryptography": "~=44.0.0",
         "pyparsing": ">=3.1.0,<4",
+        "rich": ">=14.0.0",
         "idf-component-manager": "~=2.0.1",
         "esp-idf-kconfig": "~=2.5.0"
     }
